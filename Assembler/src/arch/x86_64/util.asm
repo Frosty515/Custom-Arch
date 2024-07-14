98db6e887ee04d2b6a8e850884abb3841a3f6f58
Frosty515 <frosty515yt@gmail.com>
2024-07-14 20:35:55 +1000

    Add license info to all files

--- Assembler/src/arch/x86_64/util.asm
+++ Assembler/src/arch/x86_64/util.asm
@@ -1,4 +1,4 @@
-; Copyright (©) 2023  Frosty515
+; Copyright (©) 2023-2024  Frosty515
 ; 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
