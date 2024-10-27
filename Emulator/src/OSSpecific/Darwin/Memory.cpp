c1fe362516de3ceff7a3c9039388e0662470990e
fwpirate <186155331+fwpirate@users.noreply.github.com>
2024-10-27 09:29:26 +0100

    Added myself to the "authors"

--- Emulator/src/OSSpecific/Darwin/Memory.cpp
+++ Emulator/src/OSSpecific/Darwin/Memory.cpp
@@ -13,6 +13,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Authors
+  - fwpirate <alavik@disroot.org>
 */
 
 #include "../Memory.hpp"
